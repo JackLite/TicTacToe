a64f05df4fb1ee2ee6800c2b1003a1279e184e15
JackLite <jackkdlite@gmail.com>
2017-11-22 21:01:21 +0300

    Добавил настройки при начале новой игры

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,16 +9,28 @@ public class MenuManager : MonoBehaviour
     [SerializeField] public GameObject MainMenu;
     [SerializeField] public GameObject Options;
     [SerializeField] public GameObject Autors;
+    [SerializeField] public GameObject OptionsBackBtn;
+    [SerializeField] public GameObject OptionsStartBtn;
 
     private void Awake()
     { 
         Debug.Log( Screen.dpi );
         MainMenu.GetComponent<VerticalLayoutGroup>().spacing = Screen.height * .05f;
     }
-    public void ShowOptions()
+    public void ShowOptions(bool isStartGame)
     {
         MainMenu.SetActive(false);
         Options.SetActive(true);
+        if(isStartGame)
+        {
+            OptionsBackBtn.SetActive(false);
+            OptionsStartBtn.SetActive(true);
+        }
+        else
+        {
+            OptionsBackBtn.SetActive(true);
+            OptionsStartBtn.SetActive(false);
+        }
     }
 
     public void ReturnFromOptions()
